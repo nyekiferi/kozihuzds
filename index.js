0fbdb79619ab9d1f007581b912f07990ba803bda
hallabalint <59656917+hallabalint@users.noreply.github.com>
2022-12-21 23:18:25 +0100

    debug and new random logic

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const tagok = ["Seszti", "Dori", "Mariann", "Vanda", "Laci", "Zsolt atya", "Feca", "Balint", "Mate"]
 
-const huzhatok = ["Seszti", "Dori", "Mariann", "Vanda", "Laci", "Zsolt atya", "Feca", "Balint", "Mate"]
-
 const express = require('express')
 const app = express()
 
@@ -9,24 +7,38 @@ app.use(express.static(__dirname))
 app.use(express.urlencoded({extended: true}))
 app.listen(8080)
 
-var huzas
+var map;
+
+//random generátor
+try {
+    lista = require('./lista.obj')
+    map = new Map(lista);
+} catch (e) {
+    console.log(e)
+    map = new Map();
+    const tagok_random = tagok.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < tagok_random.length-1; i++) {
+        map.set(tagok_random[i], tagok_random[i+1]);
+    }
+    map.set(tagok_random[tagok_random.length-1], tagok_random[0]);
+    const obj = Object.fromEntries(map1);
+    const fs = require('fs');
+    fs.writeFile('lista.obj', JSON.stringify(obj), (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+}
+
 
 app.post('/huzas', function (req, res) {
     var index = Math.floor(Math.random() * huzhatok.length)
     var huzott = huzhatok[index]
     var nev = req.body.drawName
-    if(huzott === nev){
-        huzas = "Magadat huztad, huzz ujat!"
-    }
-    else if (!tagok.includes(nev)){
+    if (!tagok.includes(nev)){
         huzas = "Nem vagy tagja a húzós csapatnak :,("
     }
-    else if(huzhatok.length == 0){
-        huzas = "Elfogytak a huzhato nevek!"
-    }
-    else{
-        huzhatok.splice(index, 1)
-        huzas = huzott
+    else {
+        huzott = map.get(nev);
     }
 })
 
